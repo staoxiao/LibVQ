aee35304d1bd5e15f1b6227581dc5e614fb04056
chaofan <545999961@qq.com>
2022-11-29 14:50:12 +0800

    branch demo

--- LibVQ/models/encoder_config.py
+++ LibVQ/models/encoder_config.py
@@ -5,8 +5,6 @@ class EncoderConfig():
                  is_finetune: bool = False,
                  doc_encoder_name_or_path: str = None,
                  query_encoder_name_or_path: str = None,
-                 max_doc_length: int = 256,
-                 max_query_length: int = 32,
                  **kwargs):
         """
 
@@ -20,16 +18,12 @@ class EncoderConfig():
         self.is_finetune = is_finetune
         self.doc_encoder_name_or_path = doc_encoder_name_or_path
         self.query_encoder_name_or_path = query_encoder_name_or_path
-        self.max_doc_length = max_doc_length
-        self.max_query_length = max_query_length
 
     def save(self, path: str = 'encoder_configuration.json'):
         config_dict = dict()
         config_dict['encoder_pretrained'] = self.encoder_pretrained
         config_dict['doc_encoder_name_or_path'] = self.doc_encoder_name_or_path
         config_dict['query_encoder_name_or_path'] = self.query_encoder_name_or_path
-        config_dict['max_doc_length'] = self.max_doc_length
-        config_dict['max_query_length'] = self.max_query_length
         json.dump(config_dict, open(path, 'w+'))
         print(f'save encoder_configuration to: {path}')
 
